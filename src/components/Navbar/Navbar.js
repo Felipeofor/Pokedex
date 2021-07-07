d950188b40282181e2136eae0827add95d64278d
Ramos Felipe <felipeofor@gmail.com>
2021-07-07 20:18:53 -0300

    Creacion de app

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+
+    return (
+        <nav>
+            <div>
+                Hola Navbar
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
