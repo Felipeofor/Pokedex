2ac46f7772bfdfabc83236a8a3bca041824e85dd
Ramos Felipe <felipeofor@gmail.com>
2021-07-08 23:52:01 -0300

    Tarjeta con caracteristicas del pokemon

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,26 +2,32 @@ import React, { useRef } from "react";
 import ReactDom from "react-dom";
 import Pokemon from '../Pokemon';
 import { searchPokemon } from '../../api';
-
-
 export const Modal = ({ setShowModal, elements}) => {
+  console.log(elements)
   // close the modal when clicking outside the modal.
   const modalRef = useRef();
- 
   const closeModal = (e) => {
     if (e.target === modalRef.current) {
       setShowModal(false);
     }
   };
-
   //render the modal JSX in the portal div.
   return ReactDom.createPortal(
-
     <div className="container" ref={modalRef} onClick={closeModal}>
       <div className="modal">
-          
-         <div>{}</div>
-    </div></div>
+        <div className="description">
+          <div><img src={elements.pokemon.sprites.front_default} alt={elements.pokemon.name} /></div>
+         <div>Nombre: {elements.pokemon.name}</div> 
+         <div>Peso: {elements.pokemon.weight}</div>
+         <div>Altura: {elements.pokemon.height}</div>
+         <div>Experiencia: {elements.pokemon.base_experience}</div> 
+          {elements.pokemon.types.map(({type}) => {
+           return <div>Tipo: {type.name}</div>
+           })
+         }
+         </div> 
+      </div>
+      </div>
     ,document.getElementById("portal")
   );
 };
